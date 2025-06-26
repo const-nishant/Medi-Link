cdf2deb257c2dd15a2faaa9a21d90d81ddd13b06
const-nishant <nishantspatil04@gmail.com>
2025-06-26 12:13:30 +0530

    feat: add CORS middleware and update dependencies

--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,18 @@ import authRouter from "./routes/auth.route.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import appointmentRouter from "./routes/appoinment.route.js";
 import userRouter from "./routes/user.route.js";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "*", //  change this in production
+    credentials: true,
+  })
+);
 
 // Import routes
 app.use("/api/v1/auth", authRouter);
